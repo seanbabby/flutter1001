f33a0e94f65ac84ba44e9d38c446aaee3b8a1462
Bear <a.9876542@hotmail.com>
2023-10-12 17:01:33 +0800

    working copy

--- lib/qpp_sample/page/commodity_info_page.dart
+++ lib/qpp_sample/page/commodity_info_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hello_mdfk/qpp_sample/common/base_app_bar.dart';
+import 'package:hello_mdfk/qpp_sample/common/base_body.dart';
 import 'package:hello_mdfk/qpp_sample/common/qpp_color.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -25,118 +26,114 @@ class CommodityInfoPage extends ConsumerWidget {
         elevation: 0,
         title: const BaseAppBar(),
       ),
-      //使用 container 做 body 容器
-      body: Container(
-        // body 容器 bg
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/desktop-bg-kv.webp'),
-                fit: BoxFit.cover)),
-        child: ListView(children: [
-          // 上方資料卡片容器
-          Card(
-            // 切子元件超出範圍
-            clipBehavior: Clip.hardEdge,
-            semanticContainer: false,
-            margin: const EdgeInsets.fromLTRB(80, 60, 80, 40),
-            color: QppColor.prussianBlue,
-            shape: RoundedRectangleBorder(
-              // 圓角參數
-              borderRadius: BorderRadius.circular(8),
-            ),
-            // Card 陰影
-            elevation: 0,
-            child: Column(children: [
-              // 資料區 上半部
-              Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                padding: const EdgeInsets.only(top: 80, bottom: 30),
-                width: double.infinity,
-                // 上半部 bg
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            'assets/desktop-pic-commodity-largepic-sample-general.webp'),
-                        fit: BoxFit.cover)),
-                child: Column(children: [
-                  // 物品 icon
-                  ClipOval(
-                    child: Image.network(
-                      'https://storage.googleapis.com/qpp_blockchain/Item/9E56D46E4848CD1BBF82A8ADA053FF68806193A204F47058B2FB87AB0C32288C_109555_Image1.png?v=1683259489078672',
-                      errorBuilder: (context, error, stackTrace) {
-                        return SvgPicture.asset(
-                          'desktop-pic-commodity-avatar-default.svg',
-                        );
-                      },
-                      width: 100,
-                      filterQuality: FilterQuality.high,
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 45,
-                  ),
-                  // 物品名稱
-                  const Text(
-                    'item item item',
-                    style: TextStyle(fontSize: 30, color: QppColor.white),
-                  ),
-                ]),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Table(
-                  columnWidths: const {
-                    0: FixedColumnWidth(180.0),
-                  },
-                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    _itemCategoryTableRow(property: '類別', value: '456'),
-                    _itemCreatorTableRow(property: '創建者', value: '456'),
-                    _itemDescriptionTableRow(
-                        property: '說明',
-                        value:
-                            'https://storage.googleapis.com/qpp_blockchain/Item/9E56D46E4848CD1BBF82A8ADA053FF68806193A204F47058B2FB87AB0C32288C_109555_Image1.png?v=1683259489078672'),
-                  ],
-                ),
-              )
-            ]),
-          ),
-          // QR Code
-          Center(
-            child: Card(
-              margin: const EdgeInsets.only(bottom: 15),
-              // 切子元件超出範圍
-              clipBehavior: Clip.hardEdge,
-              shape: RoundedRectangleBorder(
-                // 圓角參數
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: QrImageView(
-                backgroundColor: QppColor.white,
-                data:
-                    'https://qpptec.com/app/commodity_info?commodityID=109555&sharer=SUV5G42V&lang=zh_TW&openExternalBrowser=1&action=stay',
-                size: 150,
-              ),
-            ),
-          ),
-          // 下方
-          const Padding(
-            padding: EdgeInsets.only(bottom: 50),
-            child: Text(
-              '掃描條碼從QPP中開啟',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 15,
-                  color: QppColor.indianYellow,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
-        ]),
-      ),
+      body: BaseBody(child: _commodityInfo()),
     );
   }
+}
+
+Widget _commodityInfo() {
+  return ListView(children: [
+    // 上方資料卡片容器
+    Card(
+      // 切子元件超出範圍
+      clipBehavior: Clip.hardEdge,
+      semanticContainer: false,
+      margin: const EdgeInsets.fromLTRB(80, 60, 80, 40),
+      color: QppColor.prussianBlue,
+      shape: RoundedRectangleBorder(
+        // 圓角參數
+        borderRadius: BorderRadius.circular(8),
+      ),
+      // Card 陰影
+      elevation: 0,
+      child: Column(children: [
+        // 資料區 上半部
+        Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          padding: const EdgeInsets.only(top: 80, bottom: 30),
+          width: double.infinity,
+          // 上半部 bg
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'assets/desktop-pic-commodity-largepic-sample-general.webp'),
+                  fit: BoxFit.cover)),
+          child: Column(children: [
+            // 物品 icon
+            ClipOval(
+              child: Image.network(
+                'https://storage.googleapis.com/qpp_blockchain/Item/9E56D46E4848CD1BBF82A8ADA053FF68806193A204F47058B2FB87AB0C32288C_109555_Image1.png?v=1683259489078672',
+                errorBuilder: (context, error, stackTrace) {
+                  return SvgPicture.asset(
+                    'desktop-pic-commodity-avatar-default.svg',
+                  );
+                },
+                width: 100,
+                filterQuality: FilterQuality.high,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            const SizedBox(
+              height: 45,
+            ),
+            // 物品名稱
+            const Text(
+              'item item item',
+              style: TextStyle(fontSize: 30, color: QppColor.white),
+            ),
+          ]),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Table(
+            columnWidths: const {
+              0: FixedColumnWidth(180.0),
+            },
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              _itemCategoryTableRow(property: '類別', value: '456'),
+              _itemCreatorTableRow(property: '創建者', value: '456'),
+              _itemDescriptionTableRow(
+                  property: '說明',
+                  value:
+                      'https://storage.googleapis.com/qpp_blockchain/Item/9E56D46E4848CD1BBF82A8ADA053FF68806193A204F47058B2FB87AB0C32288C_109555_Image1.png?v=1683259489078672'),
+            ],
+          ),
+        )
+      ]),
+    ),
+    // QR Code
+    Center(
+      child: Card(
+        margin: const EdgeInsets.only(bottom: 15),
+        // 切子元件超出範圍
+        clipBehavior: Clip.hardEdge,
+        shape: RoundedRectangleBorder(
+          // 圓角參數
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: QrImageView(
+          backgroundColor: QppColor.white,
+          data:
+              'https://qpptec.com/app/commodity_info?commodityID=109555&sharer=SUV5G42V&lang=zh_TW&openExternalBrowser=1&action=stay',
+          size: 150,
+        ),
+      ),
+    ),
+    // 下方
+    const Padding(
+      padding: EdgeInsets.only(bottom: 50),
+      child: Text(
+        '掃描條碼從QPP中開啟',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontSize: 15,
+            color: QppColor.indianYellow,
+            fontWeight: FontWeight.w500),
+      ),
+    ),
+  ]);
 }
 
 // 類別 item
